caa5bfe57999c4f234cc59ae3cf7deb2448a2102
Martin Simecek <msimecek@outlook.com>
2016-11-15 15:46:18 +0100

    Initial commit

---
+++ IQRF_Hackathon Console/IQRF_Hackathon Console/Data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IQRF_Hackathon_Console
+{
+    public class Data
+    {
+        public int Value { get; set; }
+        public string Device { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ IQRF_Hackathon Console/IQRF_Hackathon Console/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace IQRF_Hackathon_Console
+{
+    class Program
+    {
+        private static readonly Random _random = new Random();
+        private static CancellationTokenSource _tokenSource = new CancellationTokenSource();
+
+        readonly static Regex _regex = new Regex(@"DeviceId=(?<device>[^;]*);");
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string connString = ConfigurationManager.AppSettings["IoTHubConnString"];
+                DoWork(DeviceClient.CreateFromConnectionString(connString, TransportType.Http1), _regex.Match(connString).Groups["device"].Value);
+            }
+        }
+
+
+        private static void DoWork(DeviceClient client, string deviceName)
+        {
+            var data = new Data { Value = _random.Next(-10, DateTime.Now.Minute + 10), Device = deviceName, Time = DateTime.Now };
+            var dataString = JsonConvert.SerializeObject(data);
+            var message = new Message(Encoding.UTF8.GetBytes(dataString));
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            client.SendEventAsync(message);
+            Console.WriteLine("Message sent...");
+        }
+    }
+}
