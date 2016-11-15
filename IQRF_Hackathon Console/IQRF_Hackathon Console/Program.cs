6807209a728c8763239b4935695edd333adb8094
vjirovsky <vjirovsky@vjirovsky.cz>
2016-11-15 16:37:00 +0100

    refactoring, removed bad connection string

--- IQRF_Hackathon Console/IQRF_Hackathon Console/Program.cs
+++ IQRF_Hackathon Console/IQRF_Hackathon Console/Program.cs
@@ -16,26 +16,43 @@ namespace IQRF_Hackathon_Console
     {
         private static readonly Random _random = new Random();
         private static CancellationTokenSource _tokenSource = new CancellationTokenSource();
-
         readonly static Regex _regex = new Regex(@"DeviceId=(?<device>[^;]*);");
 
         static void Main(string[] args)
         {
             while (true)
             {
-                string connString = ConfigurationManager.AppSettings["IoTHubConnString"];
-                DoWork(DeviceClient.CreateFromConnectionString(connString, TransportType.Http1), _regex.Match(connString).Groups["device"].Value);
+
+                /* connections strings for Microsoft Azure IoT Hub
+                 * located in App.config in <appSettings> section
+                 * fill with your connection string
+                */
+                string connectionString = ConfigurationManager.AppSettings["IoTHubConnString"];
+                
+                SendToAzure(DeviceClient.CreateFromConnectionString(connectionString, TransportType.Http1), _regex.Match(connectionString).Groups["device"].Value);
             }
         }
 
 
-        private static void DoWork(DeviceClient client, string deviceName)
+        private static void SendToAzure(DeviceClient client, string deviceName)
         {
-            var data = new Data { Value = _random.Next(-10, DateTime.Now.Minute + 10), Device = deviceName, Time = DateTime.Now };
+            var data = new Data {
+                Value = _random.Next(-10, DateTime.Now.Minute + 10), 
+                Device = deviceName, Time = DateTime.Now
+            };
+
+            // Convert Data to JSON format
             var dataString = JsonConvert.SerializeObject(data);
             var message = new Message(Encoding.UTF8.GetBytes(dataString));
+
+            // wait 1 second
             Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            /* send message to Azure, do not wait for response
+             * If you want to wait for response, await this method
+             */
             client.SendEventAsync(message);
+
             Console.WriteLine("Message sent...");
         }
     }
